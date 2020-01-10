3d5bd73f98cafdbbaf55a899ddbd5b420a90db75
Yiyuan-Dong <852427139@qq.com>
2020-01-10 21:39:13 +0800

    read_write.py 我加了一个将Astock转化成一个大的二位列表的函数

--- Tools/read_write.py
+++ Tools/read_write.py
@@ -1,32 +1,37 @@
 # noinspection PyTypeChecker
 import numpy as np
-import pandas as pd
-import tushare as ts
+
+
+# import pandas as pd
+# import tushare as ts
 
 # 输出一个包含文件每行内容的列表
 def read_file(file_path):
     data_set = []
-    with open(file_path,encoding='UTF-8') as f:
+    with open(file_path, encoding='UTF-8') as f:
         line = f.readline()
         data_set.append(line)
     return data_set
 
+
 # 通过股票编码查找股票名称
 def find_name(codes):
-    with open('../Data/stocks/Acodes_names.csv',encoding='UTF-8') as f:
+    with open('../Data/stocks/Acodes_names.csv', encoding='UTF-8') as f:
         while True:
             line = f.readline().strip()
             sup = line.index(' ')
-            if(codes == line[:sup]):
-                return line[sup+1:]
+            if codes == line[:sup]:
+                return line[sup + 1:]
+
 
 def find_number(name):
     with open('../Data/stocks/Acodes_names.csv', encoding='UTF-8') as f:
         while True:
             line = f.readline().strip()
             tmp = line.index(' ')
-            if(name == line[tmp+1:]):
-                return  line[:tmp]
+            if name == line[tmp + 1:]:
+                return line[:tmp]
+
 
 def open_file_and_save(file_path, data):
     """
@@ -40,3 +45,10 @@ def open_file_and_save(file_path, data):
         with open(file_path, 'wb') as f_handle:
             np.savetxt(f_handle, data, fmt='%s', encoding="utf-8")
 
+def into_list():
+    with open('../Data/stocks/Astocks.csv', encoding='UTF-8') as f:
+        ans = []
+        ans = f.read().split('\n')
+        ans = ans[1:]
+        ans = [x.split(' ') for x in ans]
+    return ans[:-1]
